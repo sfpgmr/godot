fbb708ee4119d22dfabc27368fede55bafd7f11c
sfpgmr <sfpgmr@example.com>
2021-09-26 15:14:50 +0900

    update

---
+++ build2.sh
@@ -0,0 +1,27 @@
+
+
+scons platform=windows tools=yes target=release_debug bits=64 -j8 use_lto=yes production=yes  
+scons platform=windows tools=yes target=debug  bits=64 -j8
+scons platform=windows tools=no target=release  bits=64 -j8 use_lto=yes production=yes
+scons platform=windows tools=no target=debug  bits=64 -j8
+
+scons platform=x11 tools=yes target=release_debug  bits=64 -j8 use_llvm=yes use_lld=yes 
+scons platform=x11 tools=yes target=debug  bits=64 -j8 use_llvm=yes use_lld=yes 
+scons platform=x11 tools=no target=release  bits=64 -j8 use_llvm=yes use_lld=yes 
+scons platform=x11 tools=no target=debug  bits=64 -j8 use_llvm=yes use_lld=yes 
+
+scons platform=x11 tools=yes target=release_debug  bits=64 -j8 use_lto=yes production=yes
+scons platform=x11 tools=yes target=debug  bits=64 -j8
+scons platform=x11 tools=no target=release  bits=64 -j8 use_lto=yes production=yes
+scons platform=x11 tools=no target=debug  bits=64 -j8 
+
+# if(((godot --version) -match  "^[0-9]+\.[0-9]+\.[^\.]+")){
+#   $version = $Matches[0]
+#   $template_path = "$($env:USERPROFILE)\AppData\Roaming\Godot\templates\$($version)"
+#   if(!(Test-Path $template_path)){
+#     New-Item "$($env:USERPROFILE)\AppData\Roaming\Godot\templates\$($version)" -ItemType Directory
+#   }
+#   Copy-Item .\bin\godot.windows.debug.64.exe  "$($env:USERPROFILE)\AppData\Roaming\Godot\templates\$($version)\windows_64_debug.exe" -Force
+#   Copy-Item .\bin\godot.windows.opt.64.exe  "$($env:USERPROFILE)\AppData\Roaming\Godot\templates\$($version)\windows_64_release.exe" -Force
+# }
+# Set-Location ..
---
+++ update-repo.sh
@@ -0,0 +1,12 @@
+
+git fetch upstream
+git checkout 3.x
+git merge upstream/3.x
+git push
+git checkout master
+git merge upstream/master
+git push 
+git checkout sfpgmr
+git merge 3.x
+git push
+
